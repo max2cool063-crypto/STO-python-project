bc212954399571ae31f4e3053fb4b9ba2b982e0a
max2cool063-crypto <max2cool063@gmail.com>
2026-09-07 09:31:34 +0400

    security: route station staff page through hardened view

--- booking/urls.py
+++ booking/urls.py
@@ -16,7 +16,7 @@ from booking.views.station_appointment_detail import station_appointment_detail
 from booking.views.station_appointment_edit import station_appointment_edit
 from booking.views.station_calendar import station_day_calendar
 from booking.views.station_clients import station_clients
-from booking.views.station_staff_manage import station_staff_create_operator, station_staff_edit_profile
+from booking.views.station_staff_manage import station_staff, station_staff_create_operator, station_staff_edit_profile
 from booking.views.notifications import station_notifications, station_notifications_history, station_notification_read, station_notifications_read_all
 
 urlpatterns = [
@@ -55,7 +55,7 @@ urlpatterns = [
     path("station/<int:station_id>/schedule/", views.station_schedule, name="station_schedule"),
     path("station/<int:station_id>/slot-blocks/", views.station_slot_blocks, name="station_slot_blocks"),
     path("station/<int:station_id>/clients/", station_clients, name="station_clients"),
-    path("station/<int:station_id>/staff/", views.station_staff, name="station_staff"),
+    path("station/<int:station_id>/staff/", station_staff, name="station_staff"),
     path("station/<int:station_id>/staff/create-operator/", station_staff_create_operator, name="station_staff_create_operator"),
     path("station/<int:station_id>/staff/<int:member_id>/edit/", station_staff_edit_profile, name="station_staff_edit_profile"),
     path("station/notifications/", station_notifications, name="station_notifications"),
