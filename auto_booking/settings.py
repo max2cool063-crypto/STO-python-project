de221c138e8ce55928ac619321ff31a0f68fe415
max2cool063-crypto <max2cool063@gmail.com>
2026-09-08 03:04:11 +0400

    feat: enforce station account access on every request

--- auto_booking/settings.py
+++ auto_booking/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "booking.middleware.StationAccountAccessMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
